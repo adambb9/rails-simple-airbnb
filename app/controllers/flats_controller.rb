e06e19df62da74f9c9071bd796a3b3e9fd91de1a
Adam Biedka Whitton <adambb914@gmail.com>
2022-02-16 16:33:48 +0100

    form added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,35 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(review_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+  end
+  def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:flat).permit(:name, :address)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Flat.create!(
+    name: "#{Faker::Construction.material} #{Faker::Mountain.name}",
+    address: Faker::Address.street_name,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: rand(0..1000),
+    number_of_guests: rand(0..10)
+  )
+end
